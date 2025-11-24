dc44f9c760a6ae465357da0106ddf29080e5ad2c
juunghyun <127233049+juunghyun@users.noreply.github.com>
2025-11-24 16:15:03 +0900

    [Gold V] Title: 노드사이의 거리, Time: 76 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.cc
+++ 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.cc
@@ -1,51 +1,38 @@
-#include <bits/stdc++.h>
-
-//N개의 노드 트리
-//M개의 두 노드 쌍, 두 노드 사이의 거리 출력 gogo
-//근데 노드 사이의 가중치가 다 다르다.
-//N, M 받고 N-1개동안 간선정보 들어옴. 두 점과 거리.
-//-> 즉 각 간선의 가중치가 다름.
-//간선 다 받고 거리를 알고 싶은 M개의 노드 쌍이 주어짐.
-//둘의 거리를 출력 gogo
-//N : 2~1000, M : 1~1000, 간선의 가중치 : 1만이하 
-
-using namespace std;
+#include<bits/stdc++.h>
 
 struct Node{
     int pos;
     int dis;
 
-    bool operator<(const Node& other) const{
+    bool operator<(const Node& other)const{
         return dis>other.dis;
     }
 };
 
+using namespace std;
+
 int N, M;
-//N : 1000이므로 모든 최단거리 : 1000000000 10억? -> 플로이드 워셜 안됨.
 
 int main(){
 
     cin>>N>>M;
+    vector<pair<int, int>> nodeV[N+1];
 
-    vector<pair<int,int>> nodeV[N+1]; //간선 정보 저장
-
-    //간선정보
+    //간선 정보 저장
     for(int i = 0; i<N-1; i++){
         int node1, node2, dis;
         cin>>node1>>node2>>dis;
         nodeV[node1].push_back({node2, dis});
-        nodeV[node2].push_back({node1,dis});
+        nodeV[node2].push_back({node1, dis});
     }
 
 
     for(int i = 0; i<M; i++){
-
         int startNode, endNode;
         cin>>startNode>>endNode;
 
-        vector<int> dist(1001, 1e9);
+        vector<int> dist(N+1, 1e9);
         priority_queue<Node> pq;
-
         dist[startNode] = 0;
         pq.push({startNode, dist[startNode]});
 
@@ -53,19 +40,20 @@ int main(){
             Node tempNode = pq.top();
             pq.pop();
 
-            if(tempNode.dis>dist[tempNode.pos]) continue;
+            //노드에 있는 거리보다 갱신된 값이 있다면 ㄴㄴ
+            if(tempNode.dis>dist[tempNode.pos])continue;
 
-            for(auto k : nodeV[tempNode.pos]){ //갈 수 있는 노드정보
-                if(dist[k.first]>k.second + dist[tempNode.pos]){//해당 거리가 더 작아진다면 갱신
-                    dist[k.first] = k.second + dist[tempNode.pos];
+            //큐에서 나온 정점에서 갈 수 있는 목록 k : {갈 수 있는 정점, 거리};
+            for(auto k : nodeV[tempNode.pos]){ 
+                if(dist[k.first]>tempNode.dis + k.second){
+                    dist[k.first] = tempNode.dis + k.second;
                     pq.push({k.first, dist[k.first]});
-                } 
+                }
             }
         }
 
         cout<<dist[endNode]<<"\n";
     }
-
 
     return 0;
 }
